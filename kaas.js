37976e1e9bb8588ea4ae7f60511456c2511bcfd0
Siebe van Die <99059545@mydavinci.nl>
2019-09-24 12:22:54 +0200

    Opeens mijn  IQ is over 9000

--- kaas.js
+++ kaas.js
@@ -1,1 +1,11 @@
-document.write("kaas")
+var Geel = prompt("Is de kaas geel?")
+
+if(Geel == "ja")  {var Gaten = prompt("Zitten er gaten in?") }
+
+else{ 
+
+var Schimmel = prompt ("Heeft de kaas blauwe schimmels?")
+
+}
+
+if(Gaten == "ja")
