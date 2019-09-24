17152fb744c6e87cc592f78705666a7594ea2831
Siebe van Die <99059545@mydavinci.nl>
2019-09-24 13:45:19 +0200

    hoge IQ

--- kaas.js
+++ kaas.js
@@ -1,11 +1,29 @@
 var Geel = prompt("Is de kaas geel?")
 
-if(Geel == "ja")  {var Gaten = prompt("Zitten er gaten in?") }
+if(Geel == "ja")          {var Gaten = prompt("Zitten er gaten in?") }
 
-else{ 
+else if (Geel == "nee")   {var Schimmel = prompt ("Heeft de kaas blauwe schimmels?")}
 
-var Schimmel = prompt ("Heeft de kaas blauwe schimmels?")
 
-}
 
-if(Gaten == "ja")
+
+if(Gaten == "ja")         {var Duur = prompt ("Is de kaas belachelijk duur")}
+
+else if (Gaten == "nee")    {var Steen = prompt ("Is de kaas hard als steen")}
+
+
+
+
+
+if (Schimmel == "ja")     {var Korst = prompt ("Heeft de kaas een korst?")}
+
+else if (Schimmel == "nee")     {var Korst2 = prompt ("Heeft de kaas een korst?")}
+
+
+
+
+if (Korst == "ja")  
+	document.write("Camembert")
+
+else if (Korst == "nee")  
+	document.write("Mozzarella")
